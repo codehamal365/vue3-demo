f052e0ab063050e9418dff176994a522b6f579b6
Code hamal <30389683+codehamal365@users.noreply.github.com>
2024-06-20 22:04:00 +0800

    vue pinia persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,12 @@
 import { createPinia } from 'pinia'
 
+
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+
 const store = createPinia()
 
+// 全局配置所有store持久化
+store.use(createPersistedState({
+    auto: true,
+}))
 export default store
